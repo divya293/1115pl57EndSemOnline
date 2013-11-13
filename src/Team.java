4cfe3093c3f77b39ecf94564489889eb30efe2ba
Divya Nayak <divya_29389@yahoo.co.in>
2013-11-13 12:00:24 +0530

    updated again

--- src/Team.java
+++ src/Team.java
@@ -1,9 +1,73 @@
 
 public class Team {
 	int velocity, no_members;
-	String product_owner,scrum_master,team_members,customers;
+	String product_owner,scrum_master,team_member1,team_member2,team_member3,team_member4,team_member5,customers;
 
 
+
+	public String getProduct_owner() {
+		return product_owner;
+	}
+
+	public void setProduct_owner(String product_owner) {
+		this.product_owner = product_owner;
+	}
+
+	public String getScrum_master() {
+		return scrum_master;
+	}
+
+	public void setScrum_master(String scrum_master) {
+		this.scrum_master = scrum_master;
+	}
+
+	public String getTeam_member1() {
+		return team_member1;
+	}
+
+	public void setTeam_member1(String team_member1) {
+		this.team_member1 = team_member1;
+	}
+
+	public String getTeam_member2() {
+		return team_member2;
+	}
+
+	public void setTeam_member2(String team_member2) {
+		this.team_member2 = team_member2;
+	}
+
+	public String getTeam_member3() {
+		return team_member3;
+	}
+
+	public void setTeam_member3(String team_member3) {
+		this.team_member3 = team_member3;
+	}
+
+	public String getTeam_member4() {
+		return team_member4;
+	}
+
+	public void setTeam_member4(String team_member4) {
+		this.team_member4 = team_member4;
+	}
+
+	public String getTeam_member5() {
+		return team_member5;
+	}
+
+	public void setTeam_member5(String team_member5) {
+		this.team_member5 = team_member5;
+	}
+
+	public String getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(String customers) {
+		this.customers = customers;
+	}
 
 	public int getVelocity() {
 		return velocity;
